e47a5beb977e83eaacfec96c6e4efd7a9ff4a316
Wyatt Preul <wpreul@gmail.com>
2015-08-30 18:54:51 -0500

    More examples

---
+++ routing/example7.js
@@ -0,0 +1,18 @@
+var Hapi = require('hapi');
+
+
+var handler = function (request, reply) {
+
+    reply({ Hello: request.params.hello });
+};
+
+
+var server = new Hapi.Server();
+server.connection({ port: 8080 });
+server.route({ method: 'GET', path: '/{hello?}', handler: handler });
+server.route({ method: 'GET', path: '/{hello}', handler: handler });
+
+server.start(function () {
+
+    console.log('server started on port: ' + server.info.port);
+});
---
+++ routing/example8.js
@@ -0,0 +1,31 @@
+var Hapi = require('hapi');
+
+var users = [];
+
+var createUser = function (request, reply) {
+
+    var userId = users.length;
+    users.push(request.payload);
+
+    var userUri = request.server.lookup('retrieveUser').path.replace('{id}', userId);
+    reply().created(userUri);
+};
+
+var retrieveUser = function (request, reply) {
+
+    reply({ Hello: request.params.hello });
+};
+
+
+var server = new Hapi.Server();
+server.connection({ port: 8080 });
+server.route({ method: 'POST', path: '/user', handler: createUser });
+server.route({ method: 'GET', path: '/user/{id}', config: {
+    handler: retrieveUser,
+    id: 'retrieveUser'
+} });
+
+server.start(function () {
+
+    console.log('server started on port: ' + server.info.port);
+});
